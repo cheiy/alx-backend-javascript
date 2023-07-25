3c241f550e5824e1b51126073e31866dfaf3fe22
cheiy <gschege@gmail.com>
2023-07-25 19:30:30 +0000

    Hoisted

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
-
+  var task = false;
+  var task2 = true;
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
-  task = false;
-  task2 = true;
   return [task, task2];
 }
