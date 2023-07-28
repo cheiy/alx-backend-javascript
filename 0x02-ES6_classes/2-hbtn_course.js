f5daa6f51eee3a7a78a022de29fdfd5d31f2118a
cheiy <gschege@gmail.com>
2023-07-28 18:01:32 +0000

    hbtn_course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -14,7 +14,12 @@ class HolbertonCourse {
     }
     this._name = name;
   }
- /**
+
+  get name() {
+    return this._name;
+  }
+
+  /**
    * @param {Number} length
    */
   set length(length) {
