9374aaf1a4c98c84d6e9cf8c2418711e3d8e124c
cheiy <gschege@gmail.com>
2023-09-19 23:44:56 +0000

    Prompt

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,3 @@
+const name = prompt('Welcome to Holberton School, what is your name?');
+println(`Your name is: ${name}`);
+process.on('exit', console.log('This important software is now closing'));
